add79490c1b57182dbecabdcca415c2803dd2a66
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 15:50:39 -0700

    updated while loop to include divide, square, cube, power and mod functions

--- calculator.py
+++ calculator.py
@@ -29,4 +29,26 @@ while True:
         num1 = int(tokens[1])
         num2 = int(tokens[2])
         print multiply(num1, num2)
-        
+
+    elif tokens[0] == "/":
+        num1 = float(tokens[1])
+        num2 = float(tokens[2])
+        print divide(num1, num2)
+
+    elif tokens[0] == "square":
+        num1 = int(tokens[1])
+        print square(num1)
+
+    elif tokens[0] == "cube":
+        num1 = int(tokens[1])
+        print cube(num1)
+
+    elif tokens[0] == "pow":
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+        print power(num1, num2)
+
+    elif tokens[0] == "mod":
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
+        print mod(num1, num2)
