a98fb5271b195c9f13aa729d590f5da80c5feda5
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-25 13:03:23 -0700

    fixed token index for arithmetic

--- calculator.py
+++ calculator.py
@@ -10,33 +10,35 @@ from arithmetic import *
 # Your code goes here
 while True:
     user_input = raw_input("> ")
-    tokens = user_input.split()
+    tokens = user_input.split()  
+
+
+    if len(tokens) == 0:
+        print "Sorry, you need to input characters for the calculator to work. Please try again."
 
     # break loop if input is 'q'
-    if tokens[0][0].lower() == "q":
+    elif tokens[0][0].lower() == "q":
         print "This means you want to exit. Goodbye!"
         break
 
-
-    if len(tokens) == 1 or len(tokens) == 0:
+    elif len(tokens) == 1: 
         print "Sorry, that's not enough characters. Please try again."
-        
-    elif tokens[0] == "square" or tokens[0] == "cube":
-        num1 = float(tokens[1])
+        continue    
 
+    num1 = float(tokens[1])
+
+    if len(tokens) == 2:
+        num2 = float(tokens[2])
+        
         if tokens[0] == "square":
             print square(num1)
-        else:
+
+        elif tokens[0] == "cube":
             print cube(num1)
 
-    elif len(tokens) < 3:
-        print "Sorry, that's not enough characters. Please try again."
-
-    elif len(tokens) >= 3:   
-        num1 = float(tokens[1])
-        num2 = float(tokens[2]) 
-    
-        if tokens[0] == "+":
+    elif len(tokens) > 2:
+        
+        elif tokens[0] == "+":
             print add(num1, num2)
 
         elif tokens[0] == "-":
