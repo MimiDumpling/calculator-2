25d6fae91982a78a458429669de15ec27d502a47
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-25 12:22:53 -0700

    fixed scenarios if user doesn't input enough characters

--- calculator.py
+++ calculator.py
@@ -13,41 +13,54 @@ while True:
     tokens = user_input.split()
 
     # break loop if input is 'q'
-    if tokens[0] == "q":
+    if "q" in tokens:
+        print "This means you want to exit. Goodbye!"
         break
 
     # check len(tokens) and turn input strings into floats
-    if len(tokens) == 2:
+    if len(tokens) == 1 or len(tokens) == 0:
+        print "Sorry, that's not enough characters. Please try again."
+        
+    elif tokens[0] == "square" or tokens[0] == "cube":
         num1 = float(tokens[1])
 
-    elif len(tokens) == 3:
-        num1 = float(tokens[1])
-        num2 = float(tokens[2])
+        if tokens[0] == "square":
+            print square(num1)
+        else:
+            print cube(num1)
 
-    else:
-        print "That's not a valid input. Please try again."
 
-    # check operator and run associated function
-    if tokens[0] == "square":
-        print square(num1)
+    elif len(tokens) < 3:
+        print "Sorry, that's not enough characters. Please try again."
 
-    elif tokens[0] == "cube":
-        print cube(num1)
+    elif len(tokens) >= 3:    
+    
+        if tokens[0] == "+":
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+            print add(num1, num2)
 
-    if tokens[0] == "+":
-        print add(num1, num2)
+        elif tokens[0] == "-":
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+            print subtract(num1, num2)
 
-    elif tokens[0] == "-":
-        print subtract(num1, num2)
+        elif tokens[0] == "*":
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+            print multiply(num1, num2)
 
-    elif tokens[0] == "*":
-        print multiply(num1, num2)
+        elif tokens[0] == "/":
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+            print divide(num1, num2)
 
-    elif tokens[0] == "/":
-        print divide(num1, num2)
+        elif tokens[0] == "pow":
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+            print power(num1, num2)
 
-    elif tokens[0] == "pow":
-        print power(num1, num2)
-
-    elif tokens[0] == "mod":
-        print mod(num1, num2)
+        elif tokens[0] == "mod":
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+            print mod(num1, num2)
