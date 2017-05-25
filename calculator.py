aac0792da7e09364f485e0e32f19dd0c8a48c299
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 17:37:36 -0700

    grouped logic, fixed .split() to work with spaces and tabs, fixed 'q' break, and added an else for invalid input

--- calculator.py
+++ calculator.py
@@ -10,45 +10,44 @@ from arithmetic import *
 # Your code goes here
 while True:
     user_input = raw_input("> ")
-    tokens = user_input.split(" ")
+    tokens = user_input.split()
 
+    # break loop if input is 'q'
     if tokens[0] == "q":
         break
 
-    elif tokens[0] == "+":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
-        print add(num1, num2)
+    # check len(tokens) and turn input strings into floats
+    if len(tokens) == 2:
+        num1 = float(tokens[1])
 
-    elif tokens[0] == "-":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
-        print subtract(num1, num2)
-
-    elif tokens[0] == "*":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
-        print multiply(num1, num2)
-
-    elif tokens[0] == "/":
+    elif len(tokens) == 3:
         num1 = float(tokens[1])
         num2 = float(tokens[2])
-        print divide(num1, num2)
 
-    elif tokens[0] == "square":
-        num1 = int(tokens[1])
+    else:
+        print "That's not a valid input. Please try again."
+
+    # check operator and run associated function
+    if tokens[0] == "square":
         print square(num1)
 
     elif tokens[0] == "cube":
-        num1 = int(tokens[1])
         print cube(num1)
 
+    if tokens[0] == "+":
+        print add(num1, num2)
+
+    elif tokens[0] == "-":
+        print subtract(num1, num2)
+
+    elif tokens[0] == "*":
+        print multiply(num1, num2)
+
+    elif tokens[0] == "/":
+        print divide(num1, num2)
+
     elif tokens[0] == "pow":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print power(num1, num2)
 
     elif tokens[0] == "mod":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print mod(num1, num2)
